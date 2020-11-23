d7c7b1fb3d3f58453dc85b17cd6d06f8ff6e10aa
liufeifeng <feifeng.liu@yunmu-ai.com>
2020-11-23 18:40:58 +0800

    增加路由方法

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import { Router, Route, Switch, routerRedux } from 'dva/router';
 import IndexPage from './routes/login/IndexPage';
 import Main from './routes/main/index.js'
 import MainIndex from './routes/main/helloMain'
 import User from './routes/user/index'
-import NotFind from './routes/404/index'
-
+import NotFound from './routes/404/index'
+const { ConnectedRouter } = routerRedux;
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
@@ -14,7 +14,7 @@ function RouterConfig({ history }) {
         <Main>
           <Route path="/main" component={MainIndex} />
           {/* <Route path="/main/user" component={User} />
-          <Route path="/404" component={NotFind} /> */}
+          <Route path="/404" component={NotFound} /> */}
         </Main>
       </Switch>
     </Router>
